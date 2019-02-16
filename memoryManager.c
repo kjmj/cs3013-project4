eadf5b3caf3ad080b0debac4fb6f825efacbdfcc
Ken Morton <kmorton15@gmail.com>
2019-02-16 16:43:06 -0500

    handles user input

--- memoryManager.c
+++ memoryManager.c
@@ -1,7 +1,50 @@
 #include "memoryManager.h"
 
 int main(int argc, char *argv[]) {
-    printf("Hello World\n");
+
+    char input[16]; // store the user input
+
+    printf("Instruction? ");
+    fgets(input, sizeof(input), stdin); // read in input
+    input[strcspn(input, "\n")] = '\0'; // remove the trailing '\n' character
+
+    char *delim = ","; // users input is separated by a comma
+
+    // work through the input
+    int pid = atoi(strtok(input, delim));
+    char *instruction = strtok(NULL, delim);
+    int virtualAddress = atoi(strtok(NULL, delim));
+    int meaning = atoi(strtok(NULL, delim));
+
+    // verify input
+    verifyInput(pid, instruction, virtualAddress, meaning);
+
+    printf("pid: %d\n", pid);
+    printf("instruction: %s\n", instruction);
+    printf("virtualAddress: %d\n", virtualAddress);
+    printf("meaning: %d\n", meaning);
 
     return 0;
+}
+
+/**
+ * verify the users input
+ */
+void verifyInput(int pid, char *instruction, int virtualAddress, int meaning) {
+    if(pid < 0 || pid > 3) {
+        printf("Please enter a process_id in the range [0, 3]\n");
+        exit(1);
+    }
+    if(!(strcmp(instruction, "map") == 0 || strcmp(instruction, "store") == 0 || strcmp(instruction, "load") == 0)) {
+        printf("Please enter either map, store, or load as an instruction\n");
+        exit(1);
+    }
+    if(virtualAddress < 0 || virtualAddress > 63) {
+        printf("Please enter a virtual_address in the range [0, 63]\n");
+        exit(1);
+    }
+    if(meaning < 0 || meaning > 255) {
+        printf("Please enter a meaning in the range [0, 255]\n");
+        exit(1);
+    }
 }
--- memoryManager.h
+++ memoryManager.h
@@ -2,6 +2,10 @@
 #define memoryManager
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+int *handleInput();
+void verifyInput(int pid, char *instruction, int virtualAddress, int meaning);
 
 #endif
